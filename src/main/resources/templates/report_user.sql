b05c0b7d0b158118e068eb885939ddd433a3120a
Summer <summer.book@tapque.com>
2020-04-07 16:23:30 +0800

    s3查询变更

--- src/main/resources/templates/report_user.sql
+++ src/main/resources/templates/report_user.sql
@@ -1,7 +1,7 @@
 
 SELECT date_format(from_unixtime(ts / 1000, 'UTC'), '%Y-%m-%d') AS day,
-       coalesce(publisherid, 0)                                AS publisher_id,
-       coalesce(pubappid, 0)                                  AS pub_app_id,
+       coalesce(publisher_id, 0)                                AS publisher_id,
+       coalesce(pub_app_id, 0)                                  AS pub_app_id,
        coalesce(plat, -1)                                       AS platform,
        country,
        count(DISTINCT ip)                                       AS ip_count,
@@ -17,8 +17,8 @@ WHERE
     AND d='[(${day})]'
 GROUP BY
     date_format(from_unixtime(ts / 1000, 'UTC'), '%Y-%m-%d'),
-    coalesce(publisherid, 0),
-    coalesce(pubappid, 0),
+    coalesce(publisher_id, 0),
+    coalesce(pub_app_id, 0),
     country,
     coalesce(plat, -1)
 ;
